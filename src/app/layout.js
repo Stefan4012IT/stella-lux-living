243786e9733210d17c49569c539feef203e4aadf
stefan4012it <stefanra555@gmail.com>
2026-05-26 11:40:11 +0200

    mobile hero

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Bricolage_Grotesque, Nunito_Sans } from "next/font/google";
+import Script from "next/script";
 import { assetPath } from "@/lib/paths";
 import "@/styles/globals.scss";
+
+const GTM_ID = "GTM-WP63S5CG";
 
 const nunitoSans = Nunito_Sans({
   variable: "--font-body",
@@ -38,10 +41,28 @@ export default function RootLayout({ children }) {
       className={`${nunitoSans.variable} ${bricolageGrotesque.variable} scroll-smooth antialiased`}
       style={{
         "--hero-bg": `url(${assetPath("/images/backgrounds/hero_background_001.jpg")})`,
-        "--hero-bg-mobile": `url(${assetPath("/images/backgrounds/hero_background_mob.jpg")})`,
+        "--hero-bg-mobile": `url(${assetPath("/images/backgrounds/hero_background_mob_001.jpg")})`,
       }}
     >
-      <body>{children}</body>
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+      </Script>
+      <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
